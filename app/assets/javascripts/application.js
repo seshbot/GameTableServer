577bdee72db4a071e19077943fb69f5e03482005
Paul Cechner <git@cechner.com>
2014-01-16 20:03:56 +0900

    adding ember bootstrap stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,14 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require game_table_server
+
+// for more details see: http://emberjs.com/guides/application/
+GameTableServer = Ember.Application.create();
+
 //= require_tree .
